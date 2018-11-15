2fb2a6b69a7a6e2aa0065a89ee67ca4b5ea0f823
Mary Arzumanyan <mary.arzumanyan@gmail.com>
2018-11-14 23:26:17 -0800

    Basic game functionality added.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,11 +7,21 @@ var config = {
     messagingSenderId: "624592670627"
   };
 firebase.initializeApp(config);
-
 var db = firebase.database();
 
-var yourName = "";
-var sessionKey = "";
+var rps =
+{
+    myPlayerId: "",
+    oppId: "",
+    gameId: "",
+    myName: "",
+    oppName: "",
+    myScore: 0,
+    oppScore: 0,
+    myPick: 0,
+    oppPick: 0
+}
+
 
 function guid() {
     function s4() {
@@ -19,52 +29,124 @@ function guid() {
         .toString(16)
         .substring(1);
     }
-
     return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
   }
 
-  function createPlayer(name) {
-    var playerId = guid();
-    db.ref('players/' + playerId).set({
-        username: name
-    });
-    
-    return playerId;
+  function createPlayer() {
+    rps.myPlayerId = guid();
+    db.ref('players/' + rps.myPlayerId).set({
+        username: rps.myName
+    });   
 }
 
-function findPartner(myPlayerId) {
+function findPartner() {
     var waitingRoomRef = db.ref('waiting_room');
     waitingRoomRef.once('value', function(snapshot) {
         if (snapshot.val()) {
-            var gameId = guid();
-            waitingRoomRef.set(gameId);
-            startGameSession(gameId, myPlayerId);
+            // Someone is available to start the game
+            rps.gameId = guid();
+            waitingRoomRef.set(rps.gameId);
+            startGameSession();
         } else {
-            waitForPartner(myPlayerId);
+            waitForPartner();
         }
     });
 }
 
-function waitForPartner(myPlayerId) {
+function waitForPartner() {
     var waitingRoomRef = db.ref('waiting_room');
-    waitingRoomRef.set(myPlayerId);
+    waitingRoomRef.set(rps.myPlayerId);
     waitingRoomRef.on('value', function(snapshot) {
-        if (snapshot.val() != myPlayerId) {
+        if (snapshot.val() != rps.myPlayerId) {
             // Someone joined the game
             waitingRoomRef.off();
             waitingRoomRef.remove();
-            startGameSession(snapshot.val(), myPlayerId);
+            rps.gameId = snapshot.val();
+            startGameSession();
         }
     });
 }
 
-function startGameSession(gameId, myPlayerId) {
-    var gameSessionRef = db.ref('game_session/' + gameId);
-    gameSessionRef.child(myPlayerId).set(0);
+function startGameSession() {
+    var gameSessionRef = db.ref('game_session/' + rps.gameId);
+    gameSessionRef.child(rps.myPlayerId).set(0);
+    gameSessionRef.on('child_added', function(snapshot) {
+        if (snapshot.key != rps.myPlayerId) {
+            rps.oppId = snapshot.key;
+            gameSessionRef.off();
 
-    alert("Game Started on session " + gameId);
+            db.ref('game_session/' + rps.gameId + "/" + rps.oppId).on('value', function(snapshot){
+                if (snapshot.val() != 0) {
+                    rps.oppPick = snapshot.val();
+                    match();
+                    if(rps.myPick != 0){
+                        scoresCalculation();
+                    }
+                }
+                
+            });
+        }
+    });
+
     $("#connect").css({display: "none"});
     $("#main-container").css({display: "block"});
+}
+
+
+function match()
+{
+    if(rps.myPick === 0)
+    {
+        $("#img-status-opp").attr("src", "assets/images/picked.jpg")
+    }
+    else
+    {
+        $("#img-status-opp").attr("src", "assets/images/" + rps.oppPick + ".jpg");
+    }
+}
+
+function scoresCalculation()
+{
+    var result = "";
+    if(rps.myPick === rps.oppPick)
+    {
+        result = "Draw :-|";
+    }
+    else if(rps.myPick === "Rock" && rps.oppPick === "Scissors" || rps.myPick === "Paper" && rps.oppPick === "Rock" ||
+            rps.myPick === "Scissors" && rps.oppPick === "Paper" ){
+        rps.myScore ++ ;
+        $("#myScore").text( rps.myScore );
+        result = "You won :-)";
+    }
+    else{
+        rps.oppScore ++ ;
+        $("#oppScore").text( rps.oppScore );
+        result = "You lost :-(";
+    }
+    
+    setTimeout(function() {
+        rps.myPick = 0;
+        rps.oppPick = 0; 
+        alert(result);
+        
+        db.ref("game_session/" + rps.gameId + "/" + rps.myPlayerId).set(0);
+
+        if(rps.myScore === 3)
+        {
+            alert("You won the game !!!");
+        }
+        else if(rps.oppScore === 3)
+        {
+            alert("You lost the game");
+        }
+        else {
+            $("#buttons").attr("style", "pointer-events: auto; opacity: 1;");
+            $("#img-status-your").attr("src", "assets/images/!available.jpg");
+            $("#img-status-opp").attr("src", "assets/images/!available.jpg");
+        }
+    }, 1500);
+
+    
 }
 
 window.onload = function() {
@@ -72,23 +154,22 @@ window.onload = function() {
     $("#main-container").css({display: "none"});
 
     $("#btn-connect").on("click", function(){
-        yourName = $("#name-input").val();
-        var myPlayerId = createPlayer(yourName);
-        findPartner(myPlayerId);
-    })
+        rps.myName = $("#name-input").val();
+        createPlayer();
+        findPartner();
+    });
 
     $(".btn-rps").on("click", function(){
-        var name = $(this).attr("name");
-        if(name === "Rock")
-        {
-            $("#img-status-your").attr("src", "assets/images/Rock.jpg");
+        var pick = $(this).attr("name");
+        $("#img-status-your").attr("src", "assets/images/" + pick + ".jpg");
+
+        $("#buttons").attr("style", "pointer-events: none; opacity: 0.4;");
+        db.ref("game_session/" + rps.gameId + "/" + rps.myPlayerId).set(pick);
+        rps.myPick = pick;
+        if(rps.oppPick != 0){
+            $("#img-status-opp").attr("src", "assets/images/" + rps.oppPick + ".jpg");
+            scoresCalculation();
         }
-        else if(name === "Paper")
-        {
-            $("#img-status-your").attr("src", "assets/images/Paper.jpg");
-        }
-        else{
-            $("#img-status-your").attr("src", "assets/images/Scissors.jpg");
-        }
-    })
+
+    });
 }
